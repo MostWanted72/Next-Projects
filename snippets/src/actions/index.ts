98a204ed03a1453d05669cd509e32bbbda064423
Praveen Pagidi <praveenpagidi0@gmail.com>
2024-09-30 03:42:33 +0530

    catching in next

--- snippets/src/actions/index.ts
+++ snippets/src/actions/index.ts
@@ -1,6 +1,7 @@
 'use server'
 
 import db from "@/db"
+import { revalidatePath } from "next/cache"
 import { redirect } from "next/navigation"
 
 export async function editSnippet(id: number, code: string) {
@@ -8,6 +9,7 @@ export async function editSnippet(id: number, code: string) {
     where: { id },
     data: { code }
   })
+  revalidatePath(`/snippets/${id}`)
   redirect(`/snippets/${id}`)
 }
 
@@ -15,6 +17,7 @@ export async function deleteSnippet(id: number) {
   await db.snippet.delete({
     where: { id }
   })
+  revalidatePath('/')
   redirect('/')
 }
 
@@ -48,6 +51,8 @@ export async function createSnippet(formState: { message: string}, formData: For
       }
     }
   }
+  
+  revalidatePath('/')
   // redirect function should always outsite of try and catch, because inside try it acts like an error "NEXT_REDIRECT"
   redirect("/");
 };
