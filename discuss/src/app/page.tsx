02b533701a2e1ff6b92e74d1561b53c487e33f36
Praveen Pagidi <praveenpagidi0@gmail.com>
2024-09-30 05:38:17 +0530

    authentication github

--- discuss/src/app/page.tsx
+++ discuss/src/app/page.tsx
@@ -1,9 +1,21 @@
 import { Button } from "@nextui-org/react";
+import { signIn, signOut } from "@/actions";
+import { auth } from "@/auth";
+import Profile from "@/components/profile";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
   return (
     <div>
-      <Button>Click Me!</Button>
+      <form action={signIn}>
+        <Button type="submit">Sign In</Button>
+      </form>
+      <form action={signOut}>
+        <Button type="submit">Sign Out</Button>
+      </form>
+
+      {session?.user ? <div>Signed In</div> : <div>Signed Out</div>}
+      <Profile />
     </div>
   );
 }
