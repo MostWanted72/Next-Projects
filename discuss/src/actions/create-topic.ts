160a3a225b52f9f8a6ec16d6813fba554a6d219f
Praveen Pagidi <praveenpagidi0@gmail.com>
2024-10-02 19:42:34 +0530

    topic form validation

--- discuss/src/actions/create-topic.ts
+++ discuss/src/actions/create-topic.ts
@@ -1,7 +1,12 @@
 'use server'
 
+import type { Topic } from '@prisma/client'
 import {z} from 'zod';
 import { auth } from '@/auth';
+import { db } from '@/db';
+import { redirect } from 'next/navigation';
+import { revalidatePath } from 'next/cache';
+import paths from '@/path';
 
 interface CreateTopicFormStateType {
   errors: {
@@ -12,13 +17,15 @@ interface CreateTopicFormStateType {
 }
 
 const createTopicSchema = z.object({
-  name: z.string().min(3).regex(/^[a-z]_$/, { message: 'Must be lowercase letters or dashes without spaces'}),
+  name: z.string().min(3).regex(/^[A-Za-z]+$/, { message: 'Must be lowercase letters or dashes without spaces'}),
   description: z.string().min(10),
 })
 
 export async function createTopic(formState: CreateTopicFormStateType, formData: FormData): Promise<CreateTopicFormStateType> {
   const name = formData.get('name');
   const description = formData.get('description');
+
+  await new Promise((r) => setTimeout(r, 2500))
 
   const result = createTopicSchema.safeParse({
     name, description,
@@ -34,6 +41,31 @@ export async function createTopic(formState: CreateTopicFormStateType, formData:
     return { errors: result.error.flatten().fieldErrors}
   }
 
-  return { errors: {}}
-  // TODO: revalidate the home page
+
+  let topic: Topic;
+  try {
+    topic = await db.topic.create({
+      data: {
+        slug: result.data?.name,
+        description: result.data?.description
+      }
+    })
+  } catch(err: unknown) {
+    if (err instanceof Error) {
+      return {
+        errors: {
+          _form: [err.message]
+        }
+      }
+    } else {
+      return {
+        errors: {
+          _form: ["something went wrong"]
+        }
+      }
+    }
+  }
+
+  revalidatePath('/')
+  redirect(paths.topicShow(topic.slug))
 }
