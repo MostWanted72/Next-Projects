e474184419e1f0b416e0f7eb7fb83e3fe05b0f2c
Praveen Pagidi <praveenpagidi0@gmail.com>
2024-10-04 07:37:19 +0530

    fetch posts by topic done

--- discuss/src/app/topics/[slug]/page.tsx
+++ discuss/src/app/topics/[slug]/page.tsx
@@ -1,4 +1,6 @@
+import PostList from "@/components/posts/post-list";
 import PostCreateForm from "@/components/pots/post-create-form";
+import { fetchPostByTopicSlug } from "@/db/queries/pots";
 
 interface Props {
   params: {
@@ -12,6 +14,7 @@ export default function TopicShowPage({ params }: Props) {
     <div className="grid grid-cols-4 gap-4 p-4">
       <div className="col-span-3">
         <h1 className="text-2xl font-bold mb-2">{slug}</h1>
+        <PostList fetchData={() => fetchPostByTopicSlug(slug)} />
       </div>
       <div>
         <PostCreateForm slug={slug} />
