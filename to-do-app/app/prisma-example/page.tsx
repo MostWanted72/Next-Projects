ee31ded33f1497cb6fd5ecf93a4f523215446400
Praveen Pagidi <praveenpagidi0@gmail.com>
2024-10-09 16:27:23 +0530

    sqllite query and typescript added

--- to-do-app/app/prisma-example/page.tsx
+++ to-do-app/app/prisma-example/page.tsx
@@ -1,7 +1,37 @@
-export default function PrismaExample() {
+import prisma from "@/utills/db";
+
+interface Task {
+  id: string;
+  content: string;
+  completed: boolean;
+  createdAt: Date;
+}
+
+const prismaHandler = async (): Promise<Task[]> => {
+  await prisma.task.create({
+    data: {
+      content: "wake up",
+    },
+  });
+  const allTasks = await prisma.task.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return allTasks;
+};
+
+export default async function PrismaExample() {
+  const tasks = await prismaHandler();
   return (
     <div>
       <h1 className="text-7xl">Prisma Page</h1>
+      {tasks.map((task) => (
+        <h2 className="text-xl py-2" key={task.id}>
+          {task.content}
+        </h2>
+      ))}
     </div>
   );
 }
